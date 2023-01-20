ca14a3b75003cb49ebe83de11aa7dd62eceb8363
Sagor saha <117963424+Sagor1243@users.noreply.github.com>
2023-01-20 17:23:13 +0600

    :tada: practice html project

---
+++ example.js
@@ -0,0 +1,13 @@
+const hello = () => {
+    return `Hello I'm Sagor Saha..`
+}
+
+console.log(hello());
+
+
+console.log("😃".repeat(5));
+
+
+const sagor = 'Because I\'m happy. ';
+
+console.log(`Aagor lyrics for "Happy": ${sagor.repeat(27)}`);
